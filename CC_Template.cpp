18d21e5951f7d356e339fd5f7bb5f9b9b3c611bd
WX7 <abhishekwx7@gmail.com>
2023-05-28 23:17:08 +0530

    template

---
+++ CC_Template.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long int
+#define pb push_back
+#define eb emplace_back
+#define en "\n" 
+#define INF (ll)1e18
+#define pi 3.14159265358979323846264338327950
+
+using max_heap = priority_queue<int>;
+
+const int MOD = 1e9 + 7;
+
+void solve(){
+   int n; cin>>n;
+   vector<int> a(n);   
+   for(auto i = 0; i < n; i++) cin>>a[i];
+   
+}
+
+int32_t main(){
+	ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+	int TestCases; cin>>TestCases;
+	while(TestCases--){
+		solve();
+	}
+}
